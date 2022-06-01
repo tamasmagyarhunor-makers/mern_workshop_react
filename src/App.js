871d62d43cce57c6c487bf79a11bf58c2341d0c9
Magyar-Hunor Tamas <hunor@makers.tech>
2022-06-01 02:57:47 +0100

    Add missing requires

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+
+import CreateCar from './components/CreateCar';
+import ShowCars from './components/ShowCars';
+import ShowCarDetails from './components/ShowCarDetails';
+import UpdateCar from './components/UpdateCar';
+
 class App extends Component {
   render() {
     return (
