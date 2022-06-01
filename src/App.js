6f7bfda52f1ff9de52b359ea8f056d7ed586a373
Magyar-Hunor Tamas <hunor@makers.tech>
2022-06-01 02:04:32 +0100

    Update App.js/css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path='/' component={ShowCars} />
+          <Route path='/create-car' component={CreateCar} />
+          <Route path='/edit-car/:id' component={UpdateCar} />
+          <Route path='/show-car/:id' component={ShowCarDetails} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
